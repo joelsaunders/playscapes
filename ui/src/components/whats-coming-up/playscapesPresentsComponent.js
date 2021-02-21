8a482099a92d61b379dcea5fc25b816d042d25b4
Joel Saunders <joel.saunders@complyadvantage.com>
2021-02-21 17:05:16 +0000

    add new events

--- ui/src/components/whats-coming-up/playscapesPresentsComponent.js
+++ ui/src/components/whats-coming-up/playscapesPresentsComponent.js
@@ -42,6 +42,30 @@ const PlayscapesPresents = () => {
                     name="PlayScapes Presents... An Improvisational Look at Encounters with Privilege"
                     date="23/02/2021 18:00 GMT"
                 />
+                <EventItem
+                    link="https://www.eventbrite.co.uk/e/playscapes-presentsan-improvisational-look-at-struggling-to-find-my-voice-tickets-139724001249"
+                    image="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F124951607%2F156222267191%2F1%2Foriginal.20210202-204025?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=692f354787ce2a8dcbd5fad2dd1119c4"
+                    name="PlayScapes Presents... An Improvisational Look at Struggling to Find My Voice"
+                    date="23/03/2021 18:00 GMT"
+                />
+                <EventItem
+                    link="https://www.eventbrite.co.uk/e/playscapes-presentsan-improvisational-look-at-terrible-meetings-tickets-142387704457"
+                    image="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F126742725%2F156222267191%2F1%2Foriginal.20210221-101336?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=5ca027c0641094fd55b6b93cc2f3756f"
+                    name="PlayScapes Presents... An Improvisational Look at Terrible Meetings"
+                    date="20/04/2021 18:00 GMT"
+                />
+                <EventItem
+                    link="https://www.eventbrite.co.uk/e/playscapes-presentsan-improvisational-look-at-losing-my-temper-tickets-142387943171"
+                    image="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F126743019%2F156222267191%2F1%2Foriginal.20210221-102605?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=ea8f7c7cebe6ec07b9c1c97206544a0a"
+                    name="PlayScapes Presents... An Improvisational Look at Losing My Temper"
+                    date="25/05/2021 18:00 GMT"
+                />
+                <EventItem
+                    link="https://www.eventbrite.co.uk/e/playscapes-presentsan-improvisational-look-at-racism-tickets-142388177873"
+                    image="https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F126743307%2F156222267191%2F1%2Foriginal.20210221-103617?w=800&auto=format%2Ccompress&q=75&sharp=10&rect=0%2C0%2C2160%2C1080&s=ca871a4111f68c9f9ac04b81f7d7391e"
+                    name="PlayScapes Presents... An Improvisational Look at Racism"
+                    date="29/06/2021 18:00 GMT"
+                />
             </div>
         </div>
     </div>
